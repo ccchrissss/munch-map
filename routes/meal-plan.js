c78658a11af6313dcebe682140fd3b25b6e0206a
Chris Nelson <chrisnelson7791@gmail.com>
2024-12-26 22:00:24 -0700

    create routes/meal-plan.js, add in code from routes/todos.js and begin modifying. create routes/todos.js and add in code from todo-mvc-auth-local

---
+++ routes/meal-plan.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const mealPlanController = require('../controllers/meal-plan')
+// const todosController = require('../controllers/todos') 
+const { ensureAuth } = require('../middleware/auth')
+
+
+router.get('/', mealPlanController.getMealPlan)
+
+
+
+
+
+
+// router.get('/', ensureAuth, todosController.getTodos)
+
+// router.post('/createTodo', todosController.createTodo)
+
+// router.put('/markComplete', todosController.markComplete)
+
+// router.put('/markIncomplete', todosController.markIncomplete)
+
+// router.delete('/deleteTodo', todosController.deleteTodo)
+
+module.exports = router
