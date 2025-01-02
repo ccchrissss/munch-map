88960a1658d50c28acd6565fe268250ce9e7853d
Chris Nelson <chrisnelson7791@gmail.com>
2025-01-01 22:05:05 -0800

    feat: hide username/logout link on home page if user is not logged in.

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,51 @@
 module.exports = {
-  getIndex: (req,res)=>{
-      res.render('index.ejs')
+  getIndex: (req,res) => {
+    
+    if (req.user) {
+      console.log(req.user)
+      res.render('index.ejs',
+        {
+          user: req.user
+        }
+      )
+    } else {
+      console.log('no user logged in')
+      res.render('index.ejs',
+        {
+          user: ''
+        }
+      )
+    }
+      
   }
+
+  // getUse: async (req, res) => {
+        
+  //   // console.log('req ', req)
+  //   // console.log(res)
+    
+  //   try {
+  //     // const todoItems = await Todo.find({userId:req.user.id})
+  //     // const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
+  //     // res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+  //     /////////////////////
+      
+  //     const mealPlanDocs = await MealPlan.find({ userId: req.user.id })
+  //     // console.log('meal plan count documents: ', mealPlanDocs)
+
+  //     let mealPlanArr = Array.from(mealPlanDocs)
+  //     // console.log('meal plan docs type of: ', typeof mealPlanDocs)
+  //     // console.log('meal plan Arr: ', mealPlanArr)
+
+  //     res.render('index.ejs',
+  //       {
+  //         user: req.user
+  //       }
+  //     )
+  //   } catch(err) {
+  //     console.log(err)
+  //   }
+
+  // },
+
 }
